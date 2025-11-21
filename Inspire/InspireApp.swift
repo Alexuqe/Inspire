3afc83dfe143f2c061287858484c071a633d4626
alex <hordiah1@google.com>
2025-11-21 21:44:07 +0300

    Initial Commit

---
+++ Inspire/InspireApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspireApp.swift
+//  Inspire
+//
+//  Created by Sasha on 21.11.25.
+//
+
+import SwiftUI
+
+@main
+struct InspireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InspireTests/InspireTests.swift
@@ -0,0 +1,17 @@
+//
+//  InspireTests.swift
+//  InspireTests
+//
+//  Created by Sasha on 21.11.25.
+//
+
+import Testing
+@testable import Inspire
+
+struct InspireTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
